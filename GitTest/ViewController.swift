c913b74768276c6885775093ba57df03312d69b4
Sebastian Pelczarski <>
2023-01-22 21:07:44 +0100

    no message

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Sebastian Pelczarski on 22/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("super tekst")
+        print("kolejna zmiana")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
