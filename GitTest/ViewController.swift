80b693275e99ba350919bc84fe085c062bf519a5
Sebastian Pelczarski <>
2023-01-22 21:09:57 +0100

    no message

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         
         print("super tekst")
         print("kolejna zmiana")
-        // Do any additional setup after loading the view.
+        print("Nowy prtint")
     }
 
 
